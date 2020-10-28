692e68518961ba05304e44abb14e507129f2ffc1
YoussraHazzout <71771781+YoussraHazzout@users.noreply.github.com>
2020-10-28 12:52:54 +0100

    Update Auto.java
    Opdracht 13b. Wat issues gehad met auto.getmerk

--- src/Auto.java
+++ src/Auto.java
@@ -1,2 +1,87 @@
 public class Auto {
+
+    private int snelheid;
+    private String merk;
+    private int kilometerstand;
+    private String motorMerk;
+    private int motorKracht;
+    private int motorGewicht;
+    private double motorInhoud;
+
+    public static void main(String[] args) {
+
+        // Auto 1 Toyota
+        Auto auto = new Auto("Toyota");
+        System.out.println("Toyota");
+        System.out.println("——————————");
+        System.out.println("Snelheid: " + auto.getSnelheid() + "km/hr");
+        System.out.println("Kilometerstand:" + auto.getKilometerstand() + "km/hr");
+        System.out.println();
+
+        // Auto 2 Tesla
+        Auto Tesla = new Auto("Tesla");
+        System.out.println("Tesla");
+        System.out.println("——————————");
+        System.out.println("Snelheid: " + Tesla.getSnelheid() + "km/hr");
+        System.out.println("Kilometerstand: " + Tesla.getKilometerstand() + "km/hr");
+        System.out.println();
+
+        //Auto 3 BMW
+        Auto BMW = new Auto(20000, 0, "BMW");
+        System.out.println("BMW");
+        System.out.println("——————————");
+        System.out.println("Snelheid: " + BMW.getSnelheid() + "km/hr");
+        System.out.println("Kilometerstand: " + BMW.getKilometerstand() + "km/hr");
+        System.out.println();
+        System.out.println("Motor gegevens");
+        System.out.println(auto.motorgegevens());
+    }
+
+    public static String motorgegevens() {
+        String motorMerk = "Colombo V12";
+        System.out.println("De merk van de motor is: " + motorMerk);
+        int motorKracht = 580;
+        System.out.println("De kracht van de motor is: " + motorKracht + "pk");
+        int motorGewicht = 980;
+        System.out.println("De gewicht van de motor is: " + motorGewicht + "kg");
+        double motorInhoud = 3.8;
+        System.out.println("De inhoud van de motor is: " + motorInhoud + "L");
+        return motorMerk;
+    }
+
+    // Constructor methode
+    public Auto(String merk) {
+        snelheid = 0;
+        merk = "Toyota"; // dit werkt niet?
+        kilometerstand = 20000;
+    }
+
+    public Auto(int kilometerstand, int snelheid, String merk) {
+    }
+
+    // Getter KM stand
+    public int getKilometerstand() {
+        return kilometerstand;
+    }
+
+
+    // Getter en Setter van Snelheid
+    public int getSnelheid() {
+        return snelheid;
+    }
+
+    public void setSnelheid(int snelheid) {
+        this.snelheid = snelheid;
+    }
+
+    // Getter en Setter van Merk
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+
 }
